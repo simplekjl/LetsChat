32904a4dcaa428fe2fd054440289bc3507cc90a3
Jose L Crisostomo <jl.cs606@gmail.com>
2019-05-27 12:56:03 -0700

    Upload Image to Firebase
    Fixed POJO
    Fixed rendering image
    Upload Photo feature

--- app/src/main/java/com/simplekjl/letschat/MainActivity.kt
+++ app/src/main/java/com/simplekjl/letschat/MainActivity.kt
@@ -2,6 +2,7 @@ package com.simplekjl.letschat
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.InputFilter
@@ -14,9 +15,14 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import com.simplekjl.letschat.adapters.MessageAdapter
 import com.simplekjl.letschat.databinding.ActivityMainBinding
 import com.simplekjl.letschat.models.CustomMessage
@@ -29,11 +35,17 @@ class MainActivity : AppCompatActivity() {
     val ANONYMOUS = "anonymous"
     val DEFAULT_MSG_LENGTH_LIMIT = 1000
     val RC_SIGN_IN: Int = 12
+    val RC_PHOTO_PICKER: Int = 2
+    //firebase
     private lateinit var mFirebaseDatabase: FirebaseDatabase
     private lateinit var mMessagesDatabaseReference: DatabaseReference
+    private var mChildEventListener: ChildEventListener? = null
+    private lateinit var mFirebaseStorage: FirebaseStorage
+    private lateinit var mChatStorageReference: StorageReference
+    //activity
     private lateinit var viewBinding: ActivityMainBinding
     private lateinit var mMessagesAdapter: MessageAdapter
-    private var mChildEventListener: ChildEventListener? = null
+
 
     private lateinit var mFirebaseAuth: FirebaseAuth
     private lateinit var mAuthStateListener: FirebaseAuth.AuthStateListener
@@ -48,7 +60,11 @@ class MainActivity : AppCompatActivity() {
 
         mFirebaseDatabase = FirebaseDatabase.getInstance()
         mFirebaseAuth = FirebaseAuth.getInstance()
+        mFirebaseStorage = FirebaseStorage.getInstance()
+
+
         mMessagesDatabaseReference = mFirebaseDatabase.getReference("messages")
+        mChatStorageReference = mFirebaseStorage.reference.child("chat_photos")
 
 
         // Initialize message ListView and its adapter
@@ -62,7 +78,10 @@ class MainActivity : AppCompatActivity() {
         //Image Picker button
 
         viewBinding.photoPickerButton.setOnClickListener {
-            //TODO do something
+            var intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/jpeg"
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+            startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER)
         }
 
         //Send button
@@ -178,14 +197,44 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        // case for sign in
         if (resultCode == RC_SIGN_IN) {
             if (resultCode == Activity.RESULT_OK) {
                 Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show()
 
             } else if (resultCode == Activity.RESULT_CANCELED) {
-                Toast.makeText(this, "Signedout!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Signed out!", Toast.LENGTH_SHORT).show()
                 finish()
             }
+        } else if (requestCode == RC_PHOTO_PICKER && resultCode == Activity.RESULT_OK) {
+            var selectedImageUri: Uri = data?.data ?: Uri.EMPTY
+            var photoRef: StorageReference =
+                mChatStorageReference.child(selectedImageUri.lastPathSegment)
+            //upload the file to Firebase
+            var uploadTask = photoRef.putFile(selectedImageUri)
+
+            //register observers to listen for when the download is done or if it falls
+            val urlTask = uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation photoRef.downloadUrl
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+                    var customMessage = CustomMessage(null, userName, downloadUri.toString())
+                    mMessagesDatabaseReference.push().setValue(customMessage)
+                } else {
+                    // Handle failures
+                    Toast.makeText(
+                        applicationContext, "Due connection issues your upload will continue once your " +
+                                "ethernet connection comes back", Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
         }
     }
 
