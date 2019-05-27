c1aa9cff8a029d159837ae527b414bbeb4e9be24
Jose L Crisostomo <jl.cs606@gmail.com>
2019-05-26 20:27:23 -0700

    Signin
    Sign in added into the project

--- app/src/main/java/com/simplekjl/letschat/MainActivity.kt
+++ app/src/main/java/com/simplekjl/letschat/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.simplekjl.letschat
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.InputFilter
 import android.text.TextWatcher
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import com.simplekjl.letschat.adapters.MessageAdapter
 import com.simplekjl.letschat.databinding.ActivityMainBinding
@@ -19,13 +28,17 @@ class MainActivity : AppCompatActivity() {
 
     val ANONYMOUS = "anonymous"
     val DEFAULT_MSG_LENGTH_LIMIT = 1000
+    val RC_SIGN_IN: Int = 12
     private lateinit var mFirebaseDatabase: FirebaseDatabase
     private lateinit var mMessagesDatabaseReference: DatabaseReference
     private lateinit var viewBinding: ActivityMainBinding
     private lateinit var mMessagesAdapter: MessageAdapter
-    private lateinit var mChildEventListener : ChildEventListener
+    private var mChildEventListener: ChildEventListener? = null
 
-    private lateinit var userName: String;
+    private lateinit var mFirebaseAuth: FirebaseAuth
+    private lateinit var mAuthStateListener: FirebaseAuth.AuthStateListener
+
+    private lateinit var userName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,11 +47,12 @@ class MainActivity : AppCompatActivity() {
         userName = ANONYMOUS
 
         mFirebaseDatabase = FirebaseDatabase.getInstance()
+        mFirebaseAuth = FirebaseAuth.getInstance()
         mMessagesDatabaseReference = mFirebaseDatabase.getReference("messages")
 
 
         // Initialize message ListView and its adapter
-        val customMessages =  mutableListOf<CustomMessage>()
+        val customMessages = mutableListOf<CustomMessage>()
         mMessagesAdapter = MessageAdapter(this, R.layout.item_message, customMessages)
         viewBinding.messageListView.adapter = mMessagesAdapter
 
@@ -48,7 +62,7 @@ class MainActivity : AppCompatActivity() {
         //Image Picker button
 
         viewBinding.photoPickerButton.setOnClickListener {
-            // TODO do something
+            //TODO do something
         }
 
         //Send button
@@ -73,36 +87,125 @@ class MainActivity : AppCompatActivity() {
         //Send messages action
         viewBinding.sendButton.setOnClickListener { _ ->
 
-            var message = CustomMessage(viewBinding.messageEditText.text.toString(),userName,null)
+            var message = CustomMessage(viewBinding.messageEditText.text.toString(), userName, null)
             mMessagesDatabaseReference.push().setValue(message)
             viewBinding.messageEditText.setText("")
         }
 
-        // listener to our database
-        mChildEventListener = object : ChildEventListener{
-            override fun onCancelled(p0: DatabaseError) {
 
-            }
-
-            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
-
-            }
-
-            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
-
-            }
-
-            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
-                var customMessage  = p0.getValue(CustomMessage::class.java)
-              //  var customMessage  = p0.value as? CustomMessage
-                mMessagesAdapter.add(customMessage)
-            }
-
-            override fun onChildRemoved(p0: DataSnapshot) {
-
+        mAuthStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            var user: FirebaseUser? = firebaseAuth.currentUser
+            if (user != null) {
+                // user signed in
+                //Toast.makeText(applicationContext, "Welcome, you've signed in.", Toast.LENGTH_LONG).show()
+                onSignedInInitilized(user.displayName.toString())
+            } else {
+                startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(
+                            Arrays.asList(
+                                AuthUI.IdpConfig.EmailBuilder().build(),
+                                AuthUI.IdpConfig.GoogleBuilder().build()
+                            )
+                        ).build(), RC_SIGN_IN
+                )
             }
         }
-        // adding the listener to the reference
-        mMessagesDatabaseReference.addChildEventListener(mChildEventListener)
+    }
+
+    private fun onSignedInInitilized(name: String) {
+        userName = name
+        attachDatabaseListener()
+    }
+
+    private fun onSignedOutCleanUp() {
+        userName = ANONYMOUS
+        mMessagesAdapter.clear()
+        detachDatabaseListener()
+    }
+
+    private fun attachDatabaseListener() {
+        if (mChildEventListener == null) {
+            // listener to our database
+            mChildEventListener = object : ChildEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+
+                }
+
+                override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+
+                }
+
+                override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+
+                }
+
+                override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                    var customMessage = p0.getValue(CustomMessage::class.java)
+                    //  var customMessage  = p0.value as? CustomMessage
+                    mMessagesAdapter.add(customMessage)
+                }
+
+                override fun onChildRemoved(p0: DataSnapshot) {
+
+                }
+            }
+            // adding the listener to the reference
+            mMessagesDatabaseReference.addChildEventListener(mChildEventListener!!)
+        }
+    }
+
+    private fun detachDatabaseListener() {
+        if (mChildEventListener != null) {
+            mMessagesDatabaseReference.removeEventListener(mChildEventListener!!)
+            mChildEventListener = null
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mAuthStateListener?.let {
+            mFirebaseAuth.removeAuthStateListener(it)
+        }
+        mMessagesAdapter.clear()
+        detachDatabaseListener()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RC_SIGN_IN) {
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show()
+
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, "Signedout!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.signOut -> {
+                AuthUI.getInstance().signOut(this)
+                true
+            }
+
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
